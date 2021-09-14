b6d6ec166f01682a5269b4b140ce114ab038aed4
Rajib Mondal <mondal021002rajib@gmail.com>
2021-09-14 10:50:14 +0530

    Bubble Sort Code Implementation

---
+++ 4 Searching and Sorting/bubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array after Sorting : ");
+        System.out.println(Arrays.toString(BubbleSort(size, arr)));
+    }
+
+    static int[] BubbleSort(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
